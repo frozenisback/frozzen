06098328d4a17a7beb171caacf99a32f5ce3b911
frozenisback <k83090240@gmail.com>
2025-08-17 23:01:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import hashlib
 app = Flask(__name__)
 
 # —————— Configuration ——————
-BOT_TOKEN        = os.environ.get("BOT_TOKEN", "8481470626:AAGM1w-usYfRuSD3ca3QgtH_HZv2u5cbPuc")
+BOT_TOKEN        = os.environ.get("BOT_TOKEN", "8481470626:AAG4eo2TI2EAC2kJsb9RPRmrnGChUcQP-gQ")
 CHAT_ID          = os.environ.get("CHAT_ID",   "7634862283")
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 FILE_BASE_URL    = f"https://api.telegram.org/file/bot{BOT_TOKEN}"
